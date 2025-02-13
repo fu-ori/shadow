96fea6ec3b672369fa60ce669ca1627e9cf8a540
FU-ORI プロの職人 <yzbruno@gmail.com>
2025-02-13 14:10:35 -0300

    v0.2

--- plasma/hello-world.php
+++ plasma/hello-world.php
@@ -1,19 +1,25 @@
 <!-- custom css -->
 <style type="text/css">
-	
+	body {
+		background-image: linear-gradient(to bottom, #fff, #616161);
+	}
+	p, ul{
+		color: #86efac;
+	}
 </style>
 
 <!-- ui -->
 <section class="hero is-fullheight">
 	<div class="hero-body">
 		<div class="container">
-			<i class="ph-thin ph-ghost the-color" style="font-size: 100px;"></i>
-			<h1 class="title is-1">Hello World</h1>
+			<i class="ph-fill ph-smiley-blank dark-color" style="font-size: 150px;"></i>
+			<h1 class="title is-super">Hello World</h1>
 			<h1 class="subtitle is-5">plasma is a package of codes</h1>
-			<p>Good tools that you can to use</p>
+			<p>Good tools that you can use</p>
 			<ul>
 				<li>- bulma.css</li>
 				<li>- phosphor icons</li>
+				<li>- three.js</li>
 			</ul>
 		</div>
 	</div>
